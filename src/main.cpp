4cd824a83ae3fb0eaa36ff9bf5884232ff09f558
Gustavo Oliveira <dias.gdo@gmail.com>
2020-10-19 10:22:36 -0300

    dynamic prog solution done with matrix[][]

--- src/main.cpp
+++ src/main.cpp
@@ -50,9 +50,76 @@ std::pair<int, int> checar_menor_elemento_em_vector(std::vector<int> &vec)
 	}
 }
 
-int solucao_programacao_dinamica(std::vector<int> &vec)
+int soma_total_em_vetor(std::vector<int> &vec)
 {
-	return 0;
+	int soma_total = 0;
+
+	if (vec.empty())
+	{
+		return 0;
+	}
+
+	for (auto it = vec.begin(); it != vec.end(); ++it)
+	{
+		soma_total += *it;
+	}
+
+	return soma_total;
+}
+
+int maximo_dois_inteiros(int a, int b)
+{
+	if (a >= b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
+
+int solucao_programacao_dinamica(std::vector<int> &vec, int quantidade_diamantes)
+{
+	int soma_total_de_pesos = soma_total_em_vetor(vec);
+
+	int tamanho_y = ((soma_total_de_pesos / 2) + 1);
+	int tamanho_x = quantidade_diamantes + 1;
+
+	int arr[tamanho_x][tamanho_y];
+
+	for (int i = 0; i < tamanho_x; ++i)
+	{
+		for (int j = 0; j < tamanho_y; ++j)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	/*
+	Iteramos sobre cada peso de pedra "i" armazenado em vec e sobre cada peso "p" de 1 até soma_total/2,
+	e tentamos achar o maior peso formados pelas pedras até "i" que chega proximo ou igual a "p".
+	*/
+	for (int i = 1; i < tamanho_x; ++i)
+	{
+		for (int j = 1; j < tamanho_y; ++j)
+		{
+			if (vec[i-1] <= j)
+			{
+				int aux = vec[i-1] + arr[i-1][j-vec[i-1]];
+				arr[i][j] = maximo_dois_inteiros(arr[i-1][j], aux);
+			}
+			else
+			{
+				arr[i][j] = arr[i-1][j];
+			}
+		}
+	}
+
+	int resultado = soma_total_de_pesos - (2 * arr[tamanho_x-1][tamanho_y-1]);
+
+
+	return resultado;
 }
 
 void imprime_vetor(std::vector<int> &vec)
@@ -65,7 +132,7 @@ void imprime_vetor(std::vector<int> &vec)
 	std::cout << std::endl;
 }
 
-int solucao_divisao_e_conquista(std::vector<int> &vec)
+int calcula_todas_possibilidade_maior_ao_menor(std::vector<int> &vec)
 {
 
 	std::vector<int> vetor_auxiliar;
@@ -132,12 +199,18 @@ int main(int argc, char *argv[])
 		pesos_dos_diamantes.push_back(peso_de_um_diamante);
 	}
 
+/*
 	auto start_algorithm_1 = std::chrono::steady_clock::now();
-	std::cout << solucao_divisao_e_conquista(pesos_dos_diamantes) << std::endl;
+	std::cout << calcula_todas_possibilidade_maior_ao_menor(pesos_dos_diamantes) << std::endl;
 	auto end_algorithm_1 = std::chrono::steady_clock::now();
 	auto diff = end_algorithm_1 - start_algorithm_1;
 	std::cout << std::chrono::duration <double, std::milli>(diff).count() << std::endl;
-
+*/
+	auto start_algorithm_1 = std::chrono::steady_clock::now();
+	std::cout << solucao_programacao_dinamica(pesos_dos_diamantes, quantidade_diamantes) << std::endl;
+	auto end_algorithm_1 = std::chrono::steady_clock::now();
+	auto diff = end_algorithm_1 - start_algorithm_1;
+	std::cout << std::chrono::duration <double, std::milli>(diff).count() << std::endl;
 
 	return 0;
 }
